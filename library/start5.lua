2f2300fe2af72f6dc1aad0d92815c706db4f8d16
Catharius <72415020+Catharius@users.noreply.github.com>
2020-10-21 23:56:05 +0200

    Create start5.lua

---
+++ library/start5.lua
@@ -0,0 +1,14 @@
+--------------
+-- GLOBAL VARS
+--------------
+number_keys = {"1","2","3","4","5","6","7","8","9"}
+letter_first_row_keys = {"A","Z","E","R","T","Y","U","I","O","P"}
+letter_second_row_keys = {"Q","S","D","F","G","H","J","K","L","M"}
+letter_third_row_keys = {"W","X","C","V","B","N","[","]"}
+search_string = ""
+nbchar = 0
+
+buttons_list = {}
+sorted_container_table = {}
+selection = {}
+markers_list = {} 
