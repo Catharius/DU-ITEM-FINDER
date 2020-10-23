a3e2450c693933634e3ddf6b07d312d866d60435
Catharius <72415020+Catharius@users.noreply.github.com>
2020-10-24 01:00:01 +0200

    Update start5.lua

--- library/start5.lua
+++ library/start5.lua
@@ -4,7 +4,7 @@
 number_keys = {"1","2","3","4","5","6","7","8","9"}
 letter_first_row_keys = {"A","Z","E","R","T","Y","U","I","O","P"}
 letter_second_row_keys = {"Q","S","D","F","G","H","J","K","L","M"}
-letter_third_row_keys = {"W","X","C","V","B","N","[","]"}
+letter_third_row_keys = {"W","X","C","V","B","N","[","]",".*"}
 search_string = ""
 nbchar = 0
 
